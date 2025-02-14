106086545b48fa848921f70cefdd1a88a3351a41
barnibooo <78932363+barnibooo@users.noreply.github.com>
2025-02-14 19:04:21 +0100

    Contollers folder added

--- CMS/obj/Debug/net8.0/CMS.AssemblyInfo.cs
+++ CMS/obj/Debug/net8.0/CMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6519dc0ace80b1f32815b35ed21a054f906e942")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd198530b66bd8d303b40223fe2e937d421e2c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
