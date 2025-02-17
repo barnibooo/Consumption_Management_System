e28131e57bfe2740bdf10bf55cbf752561dde2a7
dobosig <dobosigabor2006@gmail.com>
2025-02-17 13:09:30 +0100

    Tsx files created

--- CMS/CMSReact/src/main.tsx
+++ CMS/CMSReact/src/main.tsx
@@ -1,22 +1,10 @@
-<<<<<<< Updated upstream
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Side from "./Sidebar.tsx";
->>>>>>> Stashed changes
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-<<<<<<< Updated upstream
     <App />
-  </StrictMode>,
-)
-=======
-    <Side />
   </StrictMode>
 );
->>>>>>> Stashed changes
