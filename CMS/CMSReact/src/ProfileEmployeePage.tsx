9b3de6d3260c0f36067d9465eec52469664d205d
DESKTOP-RRNN1Q8\maklu <mak.luca09@gmail.com>
2025-03-13 19:15:19 +0100

    Testing ProfileEmployeePage, minor fixes

--- CMS/CMSReact/src/ProfileEmployeePage.tsx
+++ CMS/CMSReact/src/ProfileEmployeePage.tsx
@@ -1,8 +1,21 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Box, Card, CardContent, Typography, CardActions, Button, CardHeader, Avatar, CardMedia, IconButton, Divider, TextField } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  CardHeader,
+  Avatar,
+  CardMedia,
+  IconButton,
+  Divider,
+  TextField,
+} from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const darkTheme = createTheme({
   palette: {
@@ -10,14 +23,21 @@ const darkTheme = createTheme({
   },
 });
 
+interface Employee {
+  firstName: string;
+  lastName: string;
+  username: string;
+  role: string;
+}
+
 function App() {
-  const [employee, setEmployee] = useState(null);
+  const [employee, setEmployee] = useState<Employee | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
+  /*useEffect(() => {
     axios
-      .get("https://localhost:5000/api/Employees/5")
+      .get("https://localhost:5000/api/Employees/1")
       .then((response) => {
         console.log(response.data);
         setEmployee(response.data);
@@ -36,43 +56,47 @@ function App() {
 
   if (error) {
     return <div>Error: {error}</div>;
-  }
+  }*/
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100vh',
-        bgcolor: '#202938',
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
         padding: 2,
       }}
     >
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'flex-start',
-          alignItems: 'flex-start',
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
           marginBottom: 2,
         }}
-      >
-      </Box>
+      ></Box>
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
           flexGrow: 1,
         }}
       >
         <ThemeProvider theme={darkTheme}>
-          <Card sx={{ bgcolor: "#202938", color: "#d5d6d6", width: {
-                    xs: "100%",
-                    sm: "90%",
-                    md: "70%",
-                    lg: "60%",
-                    xl: "50%",
-                  }}}>
+          <Card
+            sx={{
+              bgcolor: "#202938",
+              color: "#d5d6d6",
+              width: {
+                xs: "100%",
+                sm: "90%",
+                md: "70%",
+                lg: "60%",
+                xl: "50%",
+              },
+            }}
+          >
             <CardHeader
               avatar={
                 <Avatar sx={{ bgcolor: "#BFA181" }} aria-label="recipe">
@@ -84,7 +108,7 @@ function App() {
                   <MoreVertIcon />
                 </IconButton>
               }
-              title={`${employee?.firstName} ${employee?.lastName}`}
+              title={`Kovács Béla`}
               subheader={employee?.role}
             />
             <CardMedia
@@ -94,17 +118,72 @@ function App() {
               alt="Paella dish"
             />
             <CardContent>
-              <Divider sx={{fontFamily: "Arial"}}>Vezetéknév</Divider>
-              <Typography variant="h6" sx={{ color: 'text.secondary', marginTop: "10px", marginBottom: "10px", textIndent: "20px" }}>{employee?.lastName}</Typography>
-              <Divider sx={{fontFamily: "Arial"}}>Keresztnév</Divider>
-              <Typography variant="h6" sx={{ color: 'text.secondary', marginTop: "10px", marginBottom: "10px", textIndent: "20px" }}>{employee?.firstName}</Typography>
-              <Divider sx={{fontFamily: "Arial"}}>Felhasználónév</Divider>
-              <Typography variant="h6" sx={{ color: 'text.secondary', marginTop: "10px", marginBottom: "10px", textIndent: "20px" }}>{employee?.username}</Typography>
-              <Divider sx={{fontFamily: "Arial"}}>Beosztás</Divider>
-              <Typography variant="h6" sx={{ color: 'text.secondary', marginTop: "10px", marginBottom: "10px", textIndent: "20px"}}>{employee?.role}</Typography>
+              <Typography
+                variant="h5"
+                sx={{
+                  color: "text.secondary",
+                  marginTop: "10px",
+                  marginBottom: "30px",
+                  textIndent: "20px",
+                  fontWeight: 500,
+                }}
+              >
+                Személyes Adatok
+              </Typography>
+              <Divider sx={{ fontFamily: "Arial" }}>Vezetéknév</Divider>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "text.secondary",
+                  marginTop: "10px",
+                  marginBottom: "10px",
+                  textIndent: "20px",
+                  fontWeight: 350,
+                }}
+              >
+                Kovács
+              </Typography>
+              <Divider sx={{ fontFamily: "Arial" }}>Keresztnév</Divider>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "text.secondary",
+                  marginTop: "10px",
+                  marginBottom: "10px",
+                  textIndent: "20px",
+                  fontWeight: 350,
+                }}
+              >
+                Béla
+              </Typography>
+              <Divider sx={{ fontFamily: "Arial" }}>Felhasználónév</Divider>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "text.secondary",
+                  marginTop: "10px",
+                  marginBottom: "10px",
+                  textIndent: "20px",
+                  fontWeight: 350,
+                }}
+              >
+                kbela
+              </Typography>
+              <Divider sx={{ fontFamily: "Arial" }}>Beosztás</Divider>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "text.secondary",
+                  marginTop: "10px",
+                  marginBottom: "10px",
+                  textIndent: "20px",
+                  fontWeight: 350,
+                }}
+              >
+                Takarító
+              </Typography>
             </CardContent>
-            <CardActions>
-            </CardActions>
+            <CardActions></CardActions>
           </Card>
         </ThemeProvider>
       </Box>
