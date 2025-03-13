bc7946aa7e52f4c202700001f5dcc4c0bff0405f
Luca:) <mak.luca09@gmail.com>
2025-03-13 10:25:41 +0100

    ProfilePage modified

--- CMS/CMSReact/src/ProfilePage.tsx
+++ CMS/CMSReact/src/ProfilePage.tsx
@@ -1,39 +1,58 @@
 import { useState } from "react";
 import "./OrderPage.css";
-import { Box, Card, CardContent, Typography, CardActions, Button } from "@mui/material";
+import { Box, Card, CardContent, Typography, CardActions, Button, CardHeader, Avatar } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const bull = (
-    <Box
-      component="span"
-      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-      •
-    </Box>
-  );
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+      }}
+    >
+        <ThemeProvider theme={darkTheme}>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+                <Avatar sx={{ bgcolor: "#000" }}>N</Avatar>
+                    Word of the Day
+                </Typography>
+                <Typography variant="h5" component="div">
+                    be{bull}nev{bull}o{bull}lent
+                </Typography>
+                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+                <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                </Typography>
+                </CardContent>
+                <CardActions>
+                <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        </ThemeProvider>
+    </Box>
   );
 }
 
